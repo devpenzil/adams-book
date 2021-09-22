71db4793414a3ce2e29f039e0dfb15c7115138c8
ajoalex012 <ajoalex012@gmail.com>
2021-09-22 17:27:02 +0530

    Create : Sign up  Page

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import google from '../assets/google.png'
 import facebook from '../assets/facebook.png'
-function LoginForm() {
+function SignupForm() {
     return (
         <div>
             <div className="w-full bg-green-appgreen h-screen flex justify-center items-center">
                 <div>
-                    <div className="text-center text-5xl font-semibold">Open your Book</div>
+                    <div className="text-center text-5xl font-semibold">Create a new Book</div>
                     <div>
                         <div className="mt-6">
+                            <div className="font-semibold">Email</div>
+                            <input autoFocus className="p-2 w-full   font-semibold bg-green-appgreen border-2 border-green-appgreendark rounded-xl outline-none" type="email" name="username" id="username" />
+                        </div>
+                        <div className="mt-6">
                             <div className="font-semibold">Username</div>
-                            <input autoFocus className="p-2 w-full   font-semibold bg-green-appgreen border-2 border-green-appgreendark rounded-xl outline-none" type="text" name="username" id="username" />
+                            <input className="p-2 w-full   font-semibold bg-green-appgreen border-2 border-green-appgreendark rounded-xl outline-none" type="text" name="username" id="username" />
                         </div>
                         <div className="mt-6">
                             <div className="font-semibold">Password</div>
                             <input className="p-2 w-full  font-semibold bg-green-appgreen border-2 border-green-appgreendark rounded-xl outline-none" type="password" name="username" id="username" />
                         </div>
                         <div className="mt-6 text-white">
-                            <button className="p-2 w-full font-semibold bg-green-appgreendark border-2 border-green-appgreendark rounded-xl outline-none text-base-white hover:opacity-60">Login</button>
+                            <button className="p-2 w-full font-semibold bg-green-appgreendark border-2 border-green-appgreendark rounded-xl outline-none text-base-white hover:opacity-60">Signu Up</button>
                         </div>
                     </div>
                     <div className="mt-8">
-                        <div className="text-center text-xl">or sign in with</div>
+                        <div className="text-center text-xl">or sign up with</div>
                         <div className="flex justify-center">
                             <div className="bg-base-white cursor-pointer rounded-xl m-2 p-2">
                                 <img src={google} className="w-7" alt="" />
@@ -32,7 +36,7 @@ function LoginForm() {
                         </div>
                     </div>
                     <div className="text-center mt-8">
-                        Don't have an acoount? <a href="#" className="underline">Create one</a>
+                        Already have an acoount? <a href="#" className="underline">Sign in</a>
                     </div>
                 </div>
             </div>
@@ -40,4 +44,4 @@ function LoginForm() {
     )
 }
 
-export default LoginForm
+export default SignupForm
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import HomeImg from '../components/HomeImg'
+import SignupForm from '../components/SignupForm'
 function SignupPage() {
     return (
         <div>
-            Sign up page
+            <div className="flex">
+                <div className="w-1/2">
+                    <HomeImg />
+                </div>
+                <div className="w-1/2">
+                    <SignupForm />
+                </div>
+            </div>
         </div>
     )
 }
