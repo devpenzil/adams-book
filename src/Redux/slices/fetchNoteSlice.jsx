db9849d01e355d6b05b5c5e53a33ab446b65cca9
ajoalex012 <ajoalex012@gmail.com>
2021-10-12 15:50:38 +0530

    Fix in syntax

--- src/Redux/slices/fetchNoteSlice.jsx
+++ src/Redux/slices/fetchNoteSlice.jsx
@@ -1,27 +1,29 @@
-import {createSlice} from '@reduxjs/toolkit'
-
+import { createSlice } from "@reduxjs/toolkit";
+const initvalue = {};
+const fetchngnotes = (state, action) => {
+  state.value = action.payload;
+};
+const fetsuccess = (state, action) => {
+  state.value = action.payload;
+  console.log(state.value);
+  return state;
+};
+const feterror = (state, action) => {
+  state.value = action.payload;
+  console.error(state.value);
+  return state;
+};
 export const fetchNote = createSlice({
-    name : "fetch",
-    initialState:{
-        value: {}
-    },
-    reducers:{
-        fetchingnotes: (state,action) => {
-            state.value = action.payload
-        },
-        fetchSuccess: (state,action) => {
-            state.value = action.payload
-            console.log(state.value)
-            return state
-            
-        },
-        fetchError: (state,action) => {
-            state.value = action.payload
-            console.error(state.value)
-            return state
-        }
-    }
-})
+  name: "fetch",
+  initialState: {
+    initvalue,
+  },
+  reducers: {
+    fetchingnotes: fetchngnotes,
+    fetchSuccess: fetsuccess,
+    fetchError: feterror,
+  },
+});
 
-export const {fetchingnotes, fetchSuccess, fetchError} = fetchNote.actions
-export default fetchNote.reducer
+export const { fetchingnotes, fetchSuccess, fetchError } = fetchNote.actions;
+export default fetchNote.reducer;
