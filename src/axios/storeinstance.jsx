49c4492092a0fa780d0d2b619aaa032957497593
devpenzil <ajoalex012@gmail.com>
2022-05-25 20:31:19 +0530

    clear'

--- src/axios/storeinstance.jsx
+++
@@ -1,8 +0,0 @@
-import {STORE_BASE_URL} from '../constants/constants'
-import axios from 'axios';
-// * Axios instance for Database
-const storeinstance = axios.create({
-    baseURL: STORE_BASE_URL,
-  });
-
-export default storeinstance;
